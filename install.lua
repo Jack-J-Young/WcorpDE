1d42fec5d81f5e5b92d3192d45bb7efe969ed1c1
jack young <jackjordanyoung@gmail.com>
2023-08-14 03:35:52 +0100

    force install

--- install.lua
+++ install.lua
@@ -1,5 +1,24 @@
 local updateUrl = "https://raw.githubusercontent.com/Jack-J-Young/WcorpDE/master/boot/update.lua"
 local updatePath = "/boot/update.lua"
+local updateCurrentPath = "/boot/update_current.lua"
+
+-- Function to copy the contents of one file to another using force write
+local function copyFileForce(sourcePath, destinationPath)
+    local sourceFile = fs.open(sourcePath, "r")
+    local destinationFile = fs.open(destinationPath, "a") -- Use "a" mode for force write
+
+    if sourceFile and destinationFile then
+        local content = sourceFile.readAll()
+        destinationFile.write(content)
+
+        sourceFile.close()
+        destinationFile.close()
+
+        print("Forced copied " .. sourcePath .. " to " .. destinationPath)
+    else
+        print("Failed to force copy " .. sourcePath .. " to " .. destinationPath)
+    end
+end
 
 -- Download update script
 local response = http.get(updateUrl)
@@ -15,6 +34,9 @@ if response then
 else
     print("Failed to download update script from " .. updateUrl)
 end
+
+-- Copy update.lua to update_current.lua using force write
+copyFileForce(updatePath, updateCurrentPath)
 
 -- Run the downloaded update script
 shell.run(updatePath)
