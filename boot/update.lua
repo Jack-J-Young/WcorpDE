d5a76e049eccf139ef3e4db3e505b35dbb611431
jack young <jackjordanyoung@gmail.com>
2023-08-14 02:13:18 +0100

    edit

--- boot/update.lua
+++ boot/update.lua
@@ -1,7 +1,6 @@
 local githubRepoUrl = "https://api.github.com/repos/Jack-J-Young/WcorpDE/contents/"
-local localPath = "/path/to/local/folder/"
+local localDir = "/"
 
--- Function to download a file from a URL
 local function downloadFile(url, path)
     local response = http.get(url)
     if response then
@@ -9,41 +8,37 @@ local function downloadFile(url, path)
         file.write(response.readAll())
         file.close()
         response.close()
-        return true
+        print("Downloaded: " .. path)
     else
         print("Failed to download file from " .. url)
-        return false
     end
 end
 
--- Function to download all files from a GitHub repository
 local function downloadRepositoryFiles(repoUrl, localDir)
     local response = http.get(repoUrl)
     if response then
         local contents = response.readAll()
         response.close()
-
         local fileList = textutils.unserializeJSON(contents)
         if fileList and type(fileList) == "table" then
             for _, fileData in ipairs(fileList) do
                 local fileType = fileData.type
                 local fileName = fileData.name
                 local fileUrl = fileData.download_url
-
                 local localFilePath = fs.combine(localDir, fileName)
-                downloadFile(fileUrl, localFilePath)
-                print("Downloaded: " .. fileName)
+                if fileType == "file" then
+                    downloadFile(fileUrl, localFilePath)
+                elseif fileType == "dir" then
+                    fs.makeDir(localFilePath)
+                    downloadRepositoryFiles(fileUrl, localFilePath)
+                end
             end
-            return true
         end
+    else
+        print("Failed to fetch repository contents from " .. repoUrl)
     end
-    return false
 end
 
--- Download the repository files
 print("Downloading repository files from GitHub...")
-if downloadRepositoryFiles(githubRepoUrl, localPath) then
-    print("Repository files downloaded successfully!")
-else
-    print("Failed to download repository files.")
-end
+downloadRepositoryFiles(githubRepoUrl, localDir)
+print("Repository files downloaded to root.")
